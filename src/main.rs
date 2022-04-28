f8c1dfff31b7616adfa853289f371e63538f0b8d
Jens Reidel <adrian@travitia.xyz>
2022-04-28 19:34:16 +0300

    Support SVG/XML

--- src/main.rs
+++ src/main.rs
@@ -320,7 +320,7 @@ async fn receive_icon_file(
     if let Some(document) = msg.document() {
         bot.send_message(
             msg.chat.id,
-            "Provide a name for this icon, for example youtube_music or whatsapp.",
+            "Provide a name for this icon, for example youtube_music or whatsapp. Must be lowercase and contain no special characters or spaces.",
         )
         .await?;
 
@@ -379,122 +379,212 @@ async fn receive_description(
         .await?;
 
     let file = bot.get_file(file_id).await?;
+    let extension = Path::new(&file.file_path)
+        .extension()
+        .and_then(|e| e.to_str());
 
     let mut file_bytes = Vec::new();
     bot.download_file(&file.file_path, &mut file_bytes).await?;
 
-    bot.edit_message_text(msg.chat.id, bot_msg.id, "Converting PNG to black PNM...")
-        .await?;
+    match extension {
+        Some("png") => {
+            bot.edit_message_text(msg.chat.id, bot_msg.id, "Converting PNG to black PNM...")
+                .await?;
 
-    let pnm_pixel_bytes = tokio::task::spawn_blocking(move || {
-        let mut img = load_from_memory(&file_bytes)?;
-        let mut out_img = RgbImage::new(img.width(), img.height());
+            let pnm_pixel_bytes = tokio::task::spawn_blocking(move || {
+                let mut img = load_from_memory(&file_bytes)?;
+                let mut out_img = RgbImage::new(img.width(), img.height());
 
-        for y in 0..img.height() {
-            for x in 0..img.width() {
-                // Convert any pixels that are not transparent to black
-                let pixel = img.get_pixel(x, y);
+                for y in 0..img.height() {
+                    for x in 0..img.width() {
+                        // Convert any pixels that are not transparent to black
+                        let pixel = img.get_pixel(x, y);
 
-                if pixel.0[3] > 0 {
-                    out_img.put_pixel(x, y, Rgb::<u8>([0, 0, 0]));
-                } else {
-                    out_img.put_pixel(x, y, Rgb::<u8>([255, 255, 255]));
+                        if pixel.0[3] > 0 {
+                            out_img.put_pixel(x, y, Rgb::<u8>([0, 0, 0]));
+                        } else {
+                            out_img.put_pixel(x, y, Rgb::<u8>([255, 255, 255]));
+                        }
+
+                        img.put_pixel(x, y, pixel);
+                    }
                 }
 
-                img.put_pixel(x, y, pixel);
+                let mut out = Vec::new();
+
+                out_img.write_to(
+                    &mut Cursor::new(&mut out),
+                    image::ImageOutputFormat::Pnm(PnmSubtype::Pixmap(SampleEncoding::Binary)),
+                )?;
+
+                Ok::<_, Box<dyn Error + Send + Sync>>(out)
+            })
+            .await??;
+
+            bot.edit_message_text(msg.chat.id, bot_msg.id, "Tracing PNM to SVG...")
+                .await?;
+
+            let mut potrace_proc = TokioCommand::new("potrace");
+            potrace_proc.arg("--svg");
+            potrace_proc.stdout(Stdio::piped());
+            potrace_proc.stdin(Stdio::piped());
+
+            let mut child = potrace_proc.spawn()?;
+            let mut stdin = child.stdin.take().unwrap();
+
+            stdin.write_all(&pnm_pixel_bytes).await?;
+            drop(stdin);
+
+            let op = child.wait_with_output().await?;
+
+            if !op.status.success() {
+                bot.edit_message_text(msg.chat.id, bot_msg.id, "Failed to trace PNM to SVG.")
+                    .await?;
+
+                return Ok(());
+            } else {
+                bot.edit_message_text(msg.chat.id, bot_msg.id, "Converting SVG to VD...")
+                    .await?;
             }
+
+            let svg_bytes = op.stdout;
+
+            let mut vd_proc = TokioCommand::new("svg2vd");
+            vd_proc.args(&["-i", "-", "-o", "-"]);
+            vd_proc.stdout(Stdio::piped());
+            vd_proc.stdin(Stdio::piped());
+
+            let mut child = vd_proc.spawn()?;
+            let mut stdin = child.stdin.take().unwrap();
+
+            stdin.write_all(&svg_bytes).await?;
+            drop(stdin);
+
+            let op = child.wait_with_output().await?;
+
+            if !op.status.success() {
+                bot.edit_message_text(msg.chat.id, bot_msg.id, "Failed to convert SVG to VD.")
+                    .await?;
+
+                return Ok(());
+            } else {
+                bot.edit_message_text(
+                    msg.chat.id,
+                    bot_msg.id,
+                    "Done with conversion. Here's a preview of the SVG:",
+                )
+                .await?;
+            }
+
+            let answers = InlineKeyboardMarkup::default().append_row(
+                vec!["Yes, create my request", "No, abort"]
+                    .into_iter()
+                    .map(|answer| {
+                        InlineKeyboardButton::callback(answer.to_owned(), answer.to_owned())
+                    }),
+            );
+
+            bot.send_document(
+                msg.chat.id,
+                InputFile::memory(svg_bytes).file_name("icon.svg"),
+            )
+            .caption("Please review the SVG file and if it is good, proceed!")
+            .reply_markup(answers)
+            .await?;
+
+            dialogue
+                .update(State::ConfirmingCreation {
+                    vd_bytes: op.stdout,
+                    app_path,
+                    description: description.to_owned(),
+                    icon_name,
+                })
+                .await?;
         }
+        Some("svg") => {
+            bot.edit_message_text(msg.chat.id, bot_msg.id, "Converting SVG to VD...")
+                .await?;
 
-        let mut out = Vec::new();
+            let mut vd_proc = TokioCommand::new("svg2vd");
+            vd_proc.args(&["-i", "-", "-o", "-"]);
+            vd_proc.stdout(Stdio::piped());
+            vd_proc.stdin(Stdio::piped());
 
-        out_img.write_to(
-            &mut Cursor::new(&mut out),
-            image::ImageOutputFormat::Pnm(PnmSubtype::Pixmap(SampleEncoding::Binary)),
-        )?;
+            let mut child = vd_proc.spawn()?;
+            let mut stdin = child.stdin.take().unwrap();
 
-        Ok::<_, Box<dyn Error + Send + Sync>>(out)
-    })
-    .await??;
+            stdin.write_all(&file_bytes).await?;
+            drop(stdin);
 
-    bot.edit_message_text(msg.chat.id, bot_msg.id, "Tracing PNM to SVG...")
-        .await?;
+            let op = child.wait_with_output().await?;
 
-    let mut potrace_proc = TokioCommand::new("potrace");
-    potrace_proc.arg("--svg");
-    potrace_proc.stdout(Stdio::piped());
-    potrace_proc.stdin(Stdio::piped());
+            if !op.status.success() {
+                bot.edit_message_text(msg.chat.id, bot_msg.id, "Failed to convert SVG to VD.")
+                    .await?;
 
-    let mut child = potrace_proc.spawn()?;
-    let mut stdin = child.stdin.take().unwrap();
+                return Ok(());
+            }
 
-    stdin.write_all(&pnm_pixel_bytes).await?;
-    drop(stdin);
-
-    let op = child.wait_with_output().await?;
-
-    if !op.status.success() {
-        bot.edit_message_text(msg.chat.id, bot_msg.id, "Failed to trace PNM to SVG.")
+            bot.edit_message_text(
+                msg.chat.id,
+                bot_msg.id,
+                "SVG converted to XML, uploading directly!",
+            )
             .await?;
 
-        return Ok(());
-    } else {
-        bot.edit_message_text(msg.chat.id, bot_msg.id, "Converting SVG to VD...")
+            create_icon(
+                &bot,
+                dialogue,
+                icon_name,
+                file_bytes,
+                app_path,
+                description.to_owned(),
+            )
             .await?;
+
+            bot.edit_message_text(
+                msg.chat.id,
+                bot_msg.id,
+                "Upload complete, thanks for your submission!",
+            )
+            .await?;
+        }
+        Some("xml") => {
+            bot.edit_message_text(
+                msg.chat.id,
+                bot_msg.id,
+                "Android icon XML detected, uploading directly!",
+            )
+            .await?;
+
+            create_icon(
+                &bot,
+                dialogue,
+                icon_name,
+                file_bytes,
+                app_path,
+                description.to_owned(),
+            )
+            .await?;
+
+            bot.edit_message_text(
+                msg.chat.id,
+                bot_msg.id,
+                "Upload complete, thanks for your submission!",
+            )
+            .await?;
+        }
+        _ => {
+            dialogue.exit().await?;
+
+            bot.edit_message_text(
+                msg.chat.id,
+                bot_msg.id,
+                "File format unsupported, try again!",
+            )
+            .await?;
+        }
     }
-
-    let svg_bytes = op.stdout;
-
-    let mut vd_proc = TokioCommand::new("svg2vd");
-    vd_proc.args(&["-i", "-", "-o", "-"]);
-    vd_proc.stdout(Stdio::piped());
-    vd_proc.stdin(Stdio::piped());
-
-    let mut child = vd_proc.spawn()?;
-    let mut stdin = child.stdin.take().unwrap();
-
-    stdin.write_all(&svg_bytes).await?;
-    drop(stdin);
-
-    let op = child.wait_with_output().await?;
-
-    if !op.status.success() {
-        bot.edit_message_text(msg.chat.id, bot_msg.id, "Failed to convert SVG to VD.")
-            .await?;
-
-        return Ok(());
-    } else {
-        bot.edit_message_text(
-            msg.chat.id,
-            bot_msg.id,
-            "Done with conversion. Here's a preview of the SVG:",
-        )
-        .await?;
-    }
-
-    let vd_bytes = op.stdout;
-
-    let answers = InlineKeyboardMarkup::default().append_row(
-        vec!["Yes, create my request", "No, abort"]
-            .into_iter()
-            .map(|answer| InlineKeyboardButton::callback(answer.to_owned(), answer.to_owned())),
-    );
-
-    bot.send_document(
-        msg.chat.id,
-        InputFile::memory(svg_bytes).file_name("icon.svg"),
-    )
-    .caption("Please review the SVG file and if it is good, proceed!")
-    .reply_markup(answers)
-    .await?;
-
-    dialogue
-        .update(State::ConfirmingCreation {
-            vd_bytes,
-            app_path,
-            description: description.to_owned(),
-            icon_name,
-        })
-        .await?;
 
     Ok(())
 }
@@ -508,109 +598,140 @@ async fn receive_creation_confirmation(
     if let Some(answer) = &q.data {
         if let Some(chat_id) = q.chat_id() {
             if answer == "Yes, create my request" {
-                let base = env::var("PATH_TO_ICONS_OVERLAY")?;
-                let branch_name = format!("bot/icon_{icon_name}");
-                let branch_refspec = format!("refs/heads/{branch_name}");
-                let vd_file_name = format!("themed_icon_{icon_name}.xml");
-                let commit_msg = format!("overlay: Add icon for {icon_name}");
-
-                let commit_msg_clone = commit_msg.clone();
-                let branch_name_clone = branch_name.clone();
-
-                let vd_file_path: PathBuf = [
-                    &base,
-                    "PixelLauncherIconsOverlay",
-                    "res",
-                    "drawable",
-                    &vd_file_name,
-                ]
-                .iter()
-                .collect();
-                let xml_file_path: PathBuf = [
-                    &base,
-                    "PixelLauncherIconsOverlay",
-                    "res",
-                    "xml",
-                    "grayscale_icon_map.xml",
-                ]
-                .iter()
-                .collect();
-
-                tokio::task::spawn_blocking(move || {
-                    let prev_xml = fs::read_to_string(&xml_file_path)?;
-
-                    // For whatever reason, none of the XML parsers for Rust have proper
-                    // support for serde + pretty serialization.
-                    // So for now, we add the line where it is needed.
-                    let mut lines: Vec<String> = prev_xml.lines().map(ToString::to_string).collect();
-                    let line = format!("    <icon drawable=\"@drawable/themed_icon_{icon_name}\" package=\"{app_path}\" />");
-                    lines.insert(2, line);
-                    let line_count = lines.len();
-                    lines[2..line_count - 1].sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-
-                    let new_xml = lines.join("\n");
-
-                    fs::write(vd_file_path, vd_bytes)?;
-                    fs::write(xml_file_path, new_xml)?;
-
-                    let repo = Repository::open(base)?;
-
-                    let head = repo.head()?.peel_to_commit()?;
-                    let branch = repo.branch(&branch_name, &head, true)?;
-                    repo.set_head(branch.into_reference().name().unwrap())?;
-
-                    let tree_id = {
-                        let mut index = repo.index()?;
-                        index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
-                        index.write_tree()?
-                    };
-                    let tree = repo.find_tree(tree_id)?;
-
-                    let signature = repo.signature()?;
-                    let head = repo.head()?.peel_to_commit()?;
-                    repo.commit(Some("HEAD"), &signature, &signature, &commit_msg, &tree, &[&head])?;
-                    repo.checkout_head(None)?;
-
-                    let mut push_opts = PushOptions::new();
-                    let mut callbacks = RemoteCallbacks::new();
-                    let mut remote = repo.find_remote("origin")?;
-                    callbacks.credentials(|_url, username_from_url, _allowed_type| {
-                        Cred::ssh_key(username_from_url.unwrap_or("git"), None, Path::new(&env::var("SSH_KEY").unwrap()), None)
-                    });
-                    push_opts.remote_callbacks(callbacks);
-                    remote.push(&[&branch_refspec], Some(&mut push_opts))?;
-
-                    let main_ref = repo.revparse_single("12.1")?;
-                    repo.checkout_tree(&main_ref, None)?;
-                    repo.set_head("refs/heads/12.1")?;
-
-                    Ok::<(), Box<dyn Error + Send + Sync>>(())
-                })
-                .await??;
-
-                let params = MergeRequestParams {
-                    id: OVERLAY_GITLAB_PROJECT_ID,
-                    title: commit_msg_clone,
-                    description,
-                    source_branch: branch_name_clone,
-                    target_branch: String::from("12.1"),
-                    remove_source_branch: true,
-                };
-
-                bot.inner().client()
-                    .post(format!("https://gitlab.com/api/v4/projects/{OVERLAY_GITLAB_PROJECT_ID}/merge_requests"))
-                    .header("PRIVATE-TOKEN", env::var("GITLAB_TOKEN")?)
-                    .json(&params)
-                    .send().await?;
+                create_icon(&bot, dialogue, icon_name, vd_bytes, app_path, description).await?;
 
                 bot.send_message(chat_id, "Created.").await?;
             } else {
                 bot.send_message(chat_id, "Aborting.").await?;
             }
-
-            dialogue.exit().await?;
         }
     }
+
+    Ok(())
+}
+
+async fn create_icon(
+    bot: &LeonardoBot,
+    dialogue: AppIconDialogue,
+    icon_name: String,
+    vd_bytes: Vec<u8>,
+    app_path: String,
+    description: String,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let base = env::var("PATH_TO_ICONS_OVERLAY")?;
+    let branch_name = format!("bot/icon_{icon_name}");
+    let branch_refspec = format!("refs/heads/{branch_name}");
+    let vd_file_name = format!("themed_icon_{icon_name}.xml");
+    let commit_msg = format!("overlay: Add icon for {icon_name}");
+
+    let commit_msg_clone = commit_msg.clone();
+    let branch_name_clone = branch_name.clone();
+
+    let vd_file_path: PathBuf = [
+        &base,
+        "PixelLauncherIconsOverlay",
+        "res",
+        "drawable",
+        &vd_file_name,
+    ]
+    .iter()
+    .collect();
+    let xml_file_path: PathBuf = [
+        &base,
+        "PixelLauncherIconsOverlay",
+        "res",
+        "xml",
+        "grayscale_icon_map.xml",
+    ]
+    .iter()
+    .collect();
+
+    tokio::task::spawn_blocking(move || {
+        let prev_xml = fs::read_to_string(&xml_file_path)?;
+
+        // For whatever reason, none of the XML parsers for Rust have proper
+        // support for serde + pretty serialization.
+        // So for now, we add the line where it is needed.
+        let mut lines: Vec<String> = prev_xml.lines().map(ToString::to_string).collect();
+        let line = format!(
+            "    <icon drawable=\"@drawable/themed_icon_{icon_name}\" package=\"{app_path}\" />"
+        );
+        lines.insert(2, line);
+        let line_count = lines.len();
+        lines[2..line_count - 1].sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+
+        let new_xml = lines.join("\n");
+
+        fs::write(vd_file_path, vd_bytes)?;
+        fs::write(xml_file_path, new_xml)?;
+
+        let repo = Repository::open(base)?;
+
+        let head = repo.head()?.peel_to_commit()?;
+        let branch = repo.branch(&branch_name, &head, true)?;
+        repo.set_head(branch.into_reference().name().unwrap())?;
+
+        let tree_id = {
+            let mut index = repo.index()?;
+            index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
+            index.write_tree()?
+        };
+        let tree = repo.find_tree(tree_id)?;
+
+        let signature = repo.signature()?;
+        let head = repo.head()?.peel_to_commit()?;
+        repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            &commit_msg,
+            &tree,
+            &[&head],
+        )?;
+        repo.checkout_head(None)?;
+
+        let mut push_opts = PushOptions::new();
+        let mut callbacks = RemoteCallbacks::new();
+        let mut remote = repo.find_remote("origin")?;
+        callbacks.credentials(|_url, username_from_url, _allowed_type| {
+            Cred::ssh_key(
+                username_from_url.unwrap_or("git"),
+                None,
+                Path::new(&env::var("SSH_KEY").unwrap()),
+                None,
+            )
+        });
+        push_opts.remote_callbacks(callbacks);
+        remote.push(&[&branch_refspec], Some(&mut push_opts))?;
+
+        let main_ref = repo.revparse_single("12.1")?;
+        repo.checkout_tree(&main_ref, None)?;
+        repo.set_head("refs/heads/12.1")?;
+
+        Ok::<(), Box<dyn Error + Send + Sync>>(())
+    })
+    .await??;
+
+    let params = MergeRequestParams {
+        id: OVERLAY_GITLAB_PROJECT_ID,
+        title: commit_msg_clone,
+        description,
+        source_branch: branch_name_clone,
+        target_branch: String::from("12.1"),
+        remove_source_branch: true,
+    };
+
+    bot.inner()
+        .client()
+        .post(format!(
+            "https://gitlab.com/api/v4/projects/{OVERLAY_GITLAB_PROJECT_ID}/merge_requests"
+        ))
+        .header("PRIVATE-TOKEN", env::var("GITLAB_TOKEN")?)
+        .json(&params)
+        .send()
+        .await?;
+
+    dialogue.exit().await?;
 
     Ok(())
 }
